d9cb86dd20432f8ca7e6c53c15091b846b5fa2b9
unknown <you@example.com>
2019-03-21 20:52:15 +0800

    readme上传

---
+++ sonkwo/dist/server.js
@@ -0,0 +1,45 @@
+'use strict';
+const express = require('express');
+var proxy = require('http-proxy-middleware');
+const qs = require('querystring');
+const urlpg = require('url');
+let app = express();
+
+app.use(express.static('./'))
+
+app.all('*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+	req.header('Accept', 'application/vnd.sonkwo.v5+json');
+	// 跨域请求CORS中的预请求
+	if(req.method == "OPTIONS") {
+		res.sendStatus(200); /*让options请求快速返回*/
+	} else {
+		next();
+	}
+});
+
+// local:  http://localhost:1811/jxapi/m_v1/statics/getzx.htm
+// -> https://m.jiuxian.com/jxapi/m_v1/statics/getzx.htm
+// target: https://m.jiuxian.com/m_v1/statics/getzx.htm
+let url = "https://www.sonkwo.com/api/"; //网址
+let param = ''; //参数拼接
+let transformName = ''; //转化地址
+var allUrl = '';
+
+//posts贴子信息请求
+app.use('/api/*', proxy({
+	"target": "https://www.sonkwo.com", ///api/v5/home.json
+	"changeOrigin": true,
+	"pathRewrite": {
+		"/api": "/api" //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替 比如我要调用'http://40.00.100.100:3002/user/add'，直接写‘/api/user/add’即可
+	},
+	"headers": {
+		"Accept": "application/vnd.sonkwo.v5+json"
+	}
+}));
+
+app.listen('3007', function() {
+	console.log('Server running on http://localhost:' + 3007);
+});
